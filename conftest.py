dc331a5bbfbdd9a6697bf7ba7536d05e00c503e2
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-05 22:04:16 +0300

    add conftest file

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: en, ru, ...(etc.)")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+    #пустая строка
