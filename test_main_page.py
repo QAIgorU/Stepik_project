b7a731dcf57d10289e4c41e0242ad863c1df6fdd
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-06 19:30:48 +0300

    add def test_guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -7,4 +7,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open() # открываем страницу
     page.go_to_login_page()
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
     #пустая строка
