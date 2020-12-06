c56aa23b6a46cca1a94445ff07851efc8812b03e
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-06 14:44:21 +0300

    changed base_page and main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,8 @@
-class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
-    def open(self):
-        self.browser.get(self.url)
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
