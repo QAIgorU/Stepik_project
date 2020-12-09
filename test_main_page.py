d823fd56498d9eb34cabdaa3869614d85d3d0166
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-09 22:09:31 +0300

    add elements for login_page file

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,35 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open() # открываем страницу
-    page.go_to_login_page()
+#def test_guest_can_go_to_login_page(browser):
+    #link = "http://selenium1py.pythonanywhere.com/"
+    #page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    #page.open() # открываем страницу
+    #page.go_to_login_page() # метод из main_page ищет элемент по локатору и кликает на него
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+def test_guest_can_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_url()
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+#def test_guest_should_see_login_link(browser):
+    #link = "http://selenium1py.pythonanywhere.com/"
+    #page = MainPage(browser, link)
+    #page.open()
+    #page.should_be_login_link() # сравнивает по локатору, что есть такой элемент (берет это все из main_page)
 
     #пустая строка
