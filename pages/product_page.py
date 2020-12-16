d5d475a4db376d8b8363069e8fbbc6838582a952
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-16 19:26:57 +0300

    changed asserts

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,12 +12,12 @@ class ProductPage(BasePage):
         product_name_in_store = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_STORE)
         print(product_name_in_basket.text)
         print(product_name_in_store.text)
-        assert product_name_in_store.text in product_name_in_basket.text, "Product name is no the same"
+        assert product_name_in_store.text == product_name_in_basket.text, "Product name is no the same"
 
     def product_price_is_correct(self):
         product_price_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_BASKET)
         product_prise_in_store = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_STORE)
         print(product_price_in_basket.text)
         print(product_prise_in_store.text)
-        assert product_prise_in_store.text in product_price_in_basket.text, "Price is wrong"
+        assert product_prise_in_store.text == product_price_in_basket.text, "Price is wrong"
 
