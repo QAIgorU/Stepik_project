90c9e4cbc39326e1bb7633d6c55c73bc140eab8e
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-16 19:30:57 +0300

    deleted empty line

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: en, ru, ...(etc.)")
@@ -16,5 +17,3 @@ def browser(request):
     yield browser
     #print("\nquit browser..")
     #browser.quit()
-
-    #пустая строка
