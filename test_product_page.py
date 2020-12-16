c8efc71d4f8498353e5b4021f31a3a190b0cad88
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-16 19:29:26 +0300

    add @pytest.mark.parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,18 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019" #"http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+@pytest.mark.parametrize('promo_offer', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4", "?promo=offer5", "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail(reason="fixing this bug right now")), "?promo=offer8", "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.guest_can_add_product_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(3)
+    time.sleep(2)
     page.guest_can_see_product_name()
-    time.sleep(3)
+    time.sleep(2)
     page.product_price_is_correct()
 
 
