f2e07dd6bc878584734848fb29624a50c4aa7fdc
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-18 20:11:28 +0300

    add locator SUCCESS_MESSAGE

--- pages/locators.py
+++ pages/locators.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -14,5 +16,6 @@ class ProductPageLocators():
     PRODUCT_NAME_IN_STORE = (By.CSS_SELECTOR, "#content_inner h1:first-child")
     PRODUCT_PRICE_BASKET = (By.CSS_SELECTOR, ".alert-info p:first-child strong")
     PRODUCT_PRICE_STORE = (By.CSS_SELECTOR, "#content_inner p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div.alertinner")
 
     #пустая строка
