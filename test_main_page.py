2c220c4253374220511ef7fed25ace33745c085f
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-18 20:06:54 +0300

    add blank lines

--- test_main_page.py
+++ test_main_page.py
@@ -8,11 +8,13 @@ from .pages.login_page import LoginPage
     #page.open() # открываем страницу
     #page.go_to_login_page() # метод из main_page ищет элемент по локатору и кликает на него
 
+
 def test_guest_can_see_login_url(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_url()
+
 
 def test_guest_can_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -20,11 +22,13 @@ def test_guest_can_see_login_form(browser):
     page.open()
     page.should_be_login_form()
 
+
 def test_guest_can_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
+
 
 #def test_guest_should_see_login_link(browser):
     #link = "http://selenium1py.pythonanywhere.com/"
