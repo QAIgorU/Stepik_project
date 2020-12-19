47cd9d9dec785b309701ac334f6760b78a95edf1
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-19 18:44:58 +0300

    def test_guest_can_go_to_login_page_from_product_page and def test_guest_should_see_login_link_on_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import time
 import pytest
 
@@ -41,4 +42,24 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.guest_can_add_product_to_basket()
     page.check_success_message_is_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_page()
+    
+
+
+
+
 
