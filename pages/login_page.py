4c5801fd6b59c0253efb04b67bedcccaa42e2065
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-19 18:51:38 +0300

    deleted some comments

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,16 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         #assert "who_run_the_word" in self.browser.current_url, "'login' not in current url" #для проверки что тест может отработать с ошибкой
         assert "login" in self.browser.current_url, "'login' not in current url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
 
 
