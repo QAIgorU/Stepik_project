16d28f26f99559634dd688a238e3e391052a462d
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-20 18:55:51 +0300

    add cheks for empty basket

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 #def test_guest_can_go_to_login_page(browser):
@@ -35,5 +36,14 @@ def test_guest_can_see_register_form(browser):
     #page = MainPage(browser, link)
     #page.open()
     #page.should_be_login_link() # сравнивает по локатору, что есть такой элемент (берет это все из main_page)
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.guest_can_go_to_basket_page()
+    page.guest_cant_see_product_in_basket()
+    page.guest_can_see_empty_basket()
 
     #пустая строка
