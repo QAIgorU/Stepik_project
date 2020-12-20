2fd6e3bf7bb6fb15c6298c30fabf9ce6c014051c
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-20 18:58:17 +0300

    added file basket_bage.py

---
+++ pages/basket_page.py
@@ -0,0 +1,15 @@
+from .base_page import BasePage
+from .locators import BasePageLocators
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def guest_can_go_to_basket_page(self):
+        button = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
+        button.click()
+
+    def guest_cant_see_product_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), "Product in basket is presented, but should not be"
+
+    def guest_can_see_empty_basket(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET)
