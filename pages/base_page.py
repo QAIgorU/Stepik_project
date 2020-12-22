025bc4c5ada05cfb20e7bc90ba3be07fa5e1058c
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-22 21:34:08 +0300

    def should_be_authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -43,6 +43,10 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
--- pages/locators.py
+++ pages/locators.py
@@ -23,6 +23,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
