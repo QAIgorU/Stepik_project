847816d9d7f767a1caada8c227b98873dd4e5656
Наталья Петрова <npetrova@MacBook-Pro-Natala.local>
2020-12-24 16:23:10 +0300

    deleted comments

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,7 +14,7 @@ class BasePage():
         self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
-        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented" #поменять локатор на LOGIN_LINK_INVALID чтобы проверить, что показывается сообщение об ошибке
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
